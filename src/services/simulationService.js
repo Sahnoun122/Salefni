7b21ad462ade447d376cda611fd6b45e47bf5857
sahnoun122 <khadijasahnoun70@gmail.com>
2025-11-06 14:37:21 +0100

    feat: la creations de logique dans services

---
+++ src/services/simulationService.js
@@ -0,0 +1,55 @@
+const URL = "http://localhost:3000/selfni/simulations";
+
+
+export async function obtenirSimulations(){
+    const res = await fetch(URL);
+
+    if(!res.ok)
+        throw new Error("erreur lecture de simulations")
+    return res.json()
+}
+
+export async function obtenirSimulationParId(id){
+    const res = await fetch(`${url}/${id}`);
+    if(!res.ok)
+        throw new Error("simulations non trouvee ")
+
+
+    return res.json();
+}
+
+export async function creerSimulation(simulation) {
+
+    const res = await fetch(URL,{
+        method: "POST",
+        headers : {"Content-Type" :"application/json"},
+        body : JSON.stringify(simulation)
+    });
+
+    if(!res.ok)
+        throw new Error("erreur de la creations")
+
+    return res.json()
+}
+
+export async function modifierSimulation(id , donnes){
+
+    const res = await fetch (`${URL}/${id}`,{
+        method : "PATCH",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify(donnes)
+    });
+  if(!res.ok)
+    throw new Error ("errore lors de la modifications")
+     return res.json()
+}
+
+export async function supprimerSimulation(id) {
+    const res = await fetch(`${URL}/${id}`,{
+        method : "delete"
+    })
+
+    if(!res.ok)
+        throw new Error("erreur lors de la supprission")
+      return res.json()
+}
