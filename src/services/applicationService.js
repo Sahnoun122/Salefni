f97cb9d4aaa0d554181e9ac0d1af94f3c994e0cf
sahnoun122 <khadijasahnoun70@gmail.com>
2025-11-07 11:22:41 +0100

    feat: ajouter la logiquer de l'applications dans le service

---
+++ src/services/applicationService.js
@@ -0,0 +1,28 @@
+const URL = "http://localhost:4000/applications";
+
+export  async function creerApplication (application){
+    const res = await fetch(URL , {
+        method : "POST" ,
+        headers: { "Content-Type" :"applications/json" },
+        body : JSON.stringify(application)
+    });
+    if(!res.ok) throw new Error("errore");
+    return res.json();
+};
+
+
+export async function obtenirApplications(){
+    const res = await fetch(URL);
+
+    if(!res.ok) throw new Error("errore");
+
+    return res.json();
+};
+
+export async function obtenirApplicationParId(id){
+    const res = await fetch (`${URL}/${id}`);
+
+    if(!res.ok) throw new Error("errore ");
+
+    return res.json();
+}
